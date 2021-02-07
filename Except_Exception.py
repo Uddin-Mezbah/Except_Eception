ed4ed07f82442e8e85ced291251114215c80cb90
Mezbah uddin(davide) <75944104+davide666777@users.noreply.github.com>
2021-02-07 15:01:24 +0800

    Add files via upload

---
+++ Except_Exception.py
@@ -0,0 +1,11 @@
+print("Enter davide")
+davide = int(input())
+print("Enter mexbah")
+mexbah = int(input())
+try:
+    print("The sum of these two output is",
+          int(davide) + int(mexbah))
+except Exception as e:
+    print(e)
+
+print("This line is very important")
